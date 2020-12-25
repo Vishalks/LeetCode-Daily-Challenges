c30cd8f9c73d3272d876f11fba467ce8666291f4
Vishal Sarda <Vishalks@users.noreply.github.com>
2020-12-24 23:02:06 -0500

    Add files via upload

---
+++ December/Day24.java
@@ -0,0 +1,21 @@
+//SC: O(1)
+//TC: O(n)
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        
+        ListNode cur = head, newHead = head.next;
+        
+        while (cur != null && cur.next != null) {
+            ListNode tmp = cur;
+            cur = cur.next;
+            tmp.next = cur.next;
+            cur.next = tmp;
+            cur = tmp.next;
+            if (cur != null && cur.next != null)
+                tmp.next = cur.next;
+        }
+        return newHead;
+    }
+}
